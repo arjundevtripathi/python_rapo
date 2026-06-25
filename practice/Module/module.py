3436af6a369d4b6b0fa18fca15bab7c46c6d94d1
Arjun Tripathi <sssneha056@gmail.com>
2026-06-25 12:47:46 +0530

    module

---
+++ practice/Module/module.py
@@ -0,0 +1,38 @@
+# import random
+# emp_name = ["John", "Jane", "Doe", "Smith", "Emily"]
+# res=random.choice(emp_name)
+# print("Randomly selected employee name:", res)
+
+
+
+import random
+# emp_name = ["John", "Jane", "Doe", "Smith", "Emily"]
+# weight = [2,3,1,0,4]  # Assign weights to each employee name
+# res=random.choices(emp_name, weights=weight, k=4)
+# print("Randomly selected employee name:", res)
+
+
+
+# res=random.random()*100000000
+# print("Random number between 0 and 1:", res)
+# print(int(res))
+
+
+# res=random.randint(0, 10)
+# rand_range = random.randrange(0, 10)
+# print("Random number between 0 and 10:", res)
+# print("Random number from range 0 to 10:", rand_range)
+
+
+
+# user max attempt = 6
+# each attempt random number generate
+# random number generate sum
+# fix_vaule = 150
+
+
+# ''' wap to a user play a game where user has maximum 6 attempts to guess a random number if the sum of 
+# all random number generated in each attempt is equal to the fixed value then user wins otherwise user loses
+# and print the sum of all random number generated in each attempt when user reach 150 then close game show you win otherwise you lose '''
+
+
