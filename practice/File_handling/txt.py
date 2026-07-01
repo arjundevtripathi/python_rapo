e109450d20ac748fd5fe249d0c78d227adbaf4aa
Arjun Tripathi <sssneha056@gmail.com>
2026-07-01 10:55:45 +0530

    handling

--- practice/File_handling/txt.py
+++ practice/File_handling/txt.py
@@ -3,6 +3,20 @@
 #     print("File Created")
 
 
-with open("student.txt","r") as file:
-    print(file.read())
+# with open("student.txt","r") as file:
+#     print(file.read())
     
+    
+
+name = ["a","ab"]
+for i in name:
+    with open(f"{i}.txt","w") as file:
+        file.write("this is python")
+        print("File Created")
+
+# delete file
+import os
+
+for i in name:
+    os.remove(f"{i}.txt")
+    print("File Deleted")
